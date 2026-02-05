2be3494a018d6f3017a36f3c6bbb9c6607fdecee
zjefersound <jeferson.2017317264@gmail.com>
2026-02-05 15:08:33 -0300

    try catchin

--- src/main/kotlin/TryCatch.kt
+++ src/main/kotlin/TryCatch.kt
@@ -1,2 +1,19 @@
-class TryCatch {
+fun main() {
+    println("height in cm")
+    val height = try {
+        readln().toInt()
+    } catch (e: NumberFormatException) {
+        0
+    }
+
+    try {
+        println("weight in kg")
+        val weight = readln().toInt()
+
+        val imc: Double = weight.toDouble() / (height.toDouble() / 100 * height.toDouble() / 100)
+
+        println("IMC: $imc")
+    } catch (e: Exception) {
+        println("ERROR: ${e.message}")
+    }
 }
